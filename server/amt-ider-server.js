58e4fa7cc75d9afe70b3ce24180f417ab56b0cda
BrunoSilva1978PT <bruno.1978@gmail.com>
2026-03-23 12:36:56 +0000

    Fix sdd errors: create tiny dummy floppy image to silence kernel probes

--- server/amt-ider-server.js
+++ server/amt-ider-server.js
@@ -70,11 +70,15 @@ module.exports.CreateServerIder = function () {
         obj.authState = 0;
         obj.opts = opts;
 
-        // Open the ISO file - always mount as CD-ROM for server-side IDER
-        // (AMT firmware probes CD-ROM channel first; server-side eliminates browser overhead)
+        // Open the ISO file as CD-ROM, create tiny dummy floppy to silence sdd errors
         try {
             var stats = fs.statSync(opts.isoPath);
             obj.cdrom = { size: stats.size, fd: fs.openSync(opts.isoPath, 'r') };
+            // Dummy floppy: 512 bytes of zeros - prevents kernel I/O error spam on sdd
+            var dummyPath = require('path').join(require('os').tmpdir(), 'ider-dummy-floppy.img');
+            var dummyBuf = Buffer.alloc(512);
+            fs.writeFileSync(dummyPath, dummyBuf);
+            obj.floppy = { size: 512, fd: fs.openSync(dummyPath, 'r'), dummy: false };
         } catch (e) {
             if (opts.onError) opts.onError('Cannot open file: ' + e.message);
             return null;
